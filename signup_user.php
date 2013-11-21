44e4ed7bbcf1c8d944528de72d8121446ed92e49
Cassio A. Cardoso <caugusto.cardoso@gmail.com>
2013-11-21 21:56:57 -0200

    Added homepage.html and did minor changes on signup.html and index.html

---
+++ signup_user.php
@@ -0,0 +1,25 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $university = $_POST['university'];
+    $course = $_POST['course'];
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Desesperados</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css">
+    <link rel="shortcut icon" href="img/favicon.png">
+</head>
+<body>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
